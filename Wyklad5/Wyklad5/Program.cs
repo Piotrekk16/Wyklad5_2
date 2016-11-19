c517d549272bbb48cd1169b554500dd9c5af9503
Piotrekk85 <piotr.jastr@gmail.com>
2016-11-19 19:14:20 +0100

    Add files via upload

---
+++ Wyklad5/Wyklad5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyklad5
+{
+    enum DniTygodnia { Poniedziałek=1, Wtorek, Środa, Czwartek, Piątek, Sobota, Niedziela };
+
+    enum Miesiące : byte { Sty, Lut, Mar, Kwi, Maj, Cze, Lip, Sie, Wrz, Paź, Lis, Gru };
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DniTygodnia urodziny = DniTygodnia.Poniedziałek; //zmienna urodziny będzie przyjmować wartość Poniedziałek
+            int numerDnia = (int)DniTygodnia.Czwartek; //zmienna numerDnia będzie miała wartość 4
+        }
+    }
+}
